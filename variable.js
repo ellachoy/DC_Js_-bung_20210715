9cf9818d3c76a6da3a1613e65eb9d51e6271baae
Ella Choy <egal21de@gmail.com>
2021-07-16 00:58:02 +0200

    up

---
+++ variable.js
@@ -0,0 +1,13 @@
+// variable
+
+let globalName="global name";
+
+{let name="Ella";
+console.log(name);
+name="hello";
+console.log(name);
+console.log(name);
+console.log(globalName);
+}
+console.log(name);
+console.log(globalName);
